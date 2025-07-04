b07fe55b6d4626720cb45e5cbdf39d718153fcde
Omjnr06 <oj.magbadelo@gmail.com>
2025-07-04 21:57:57 +0300

    added the makefile and the guessinggame.sh script shell

---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+
+echo "Hello, welcome to the guessing game. Please guess how many items are in the directory."
+dirsize=$(ls -1 /home/oladelemjr/finalAssignment | wc -l)
+x=1
+function calculate {
+    if [[ $1 -eq $dirsize ]]; then
+        echo "Congrats, you got the directory size correct!"
+        x=0
+    elif [[ $1 -gt $dirsize ]]; then
+        echo "The number you guessed was too high!"
+    elif [[ $1 -lt $dirsize ]]; then
+        echo "The number you guessed is too low, try again."
+    fi
+}
+while [[ $x -eq 1 ]]
+do
+    read -p "Enter your guess: " guess
+    calculate "$guess"
+done
+
---
+++ makefile
@@ -0,0 +1,6 @@
+README.md: guessinggame.sh
+echo "" >> README.md
+date >> README.md
+echo "" >> README.md
+echo "The guessinggame.sh code contains this amount one lines:" >> README.md
+wc -l < guessinggame.sh >> README.md
